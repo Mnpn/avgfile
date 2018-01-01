9cbcd7895cd4e928c0c85476cb8858650c44253e
Martin Persson <mnpn03@gmail.com>
2018-01-01 20:56:38 +0100

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+use std::io::Error;
+
 fn main() {
-    println!("Hello, world!");
+	if let Err(err) = inner_main() {
+        eprintln!("An error occured: {}", err);
+    }
+}
+
+fn inner_main() -> Result<(), Error> {
+	let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .author(crate_authors!())
+        .get_matches();
+    Ok(())
 }
