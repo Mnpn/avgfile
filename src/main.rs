b4f1b8a33e07157921d002e2c0ce48a533398fbc
Martin Persson <mnpn03@gmail.com>
2018-01-01 21:30:04 +0100

    More comments!

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ use std::io::Error;
 use std::fs::File;
 
 fn main() {
-	if let Err(err) = inner_main() {
+    // If any error would occur in inner_main(), print the error.'
+    if let Err(err) = inner_main() {
         eprintln!("An error occured: {}", err);
     }
 }
 
 fn inner_main() -> Result<(), Error> {
+	// clap app creation, with macros that read project information from Cargo.toml.
 	let matches = App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
         .author(crate_authors!())
         .arg(Arg::with_name("file")
             .help("The file to read.")
-            .required(true)
+            .required(true) // Make argument required.
             .index(1))
         .get_matches();
 
+    // Open file from argument.
     let mut file = File::open(matches.value_of("file").unwrap())?;
     
     let mut total = 0;
@@ -29,9 +32,10 @@ fn inner_main() -> Result<(), Error> {
         total += byte?;
         len += 1;
     }
-    
+
     // Prevent dividing by zero by making output 0 is the total is 0.
     let output = if len == 0 { 0 } else { total / len };
     
+    // Everything is alright!
     Ok(())
 }
