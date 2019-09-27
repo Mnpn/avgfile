551135a125863c72bd0f4c454cf9842c104e6067
Martin Persson <mnpn03@gmail.com>
2019-09-27 10:57:11 +0200

    Updated for Rust 1.26 & Updated email

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,7 @@ use clap::{App, Arg};
 use std::io::{Error, Read};
 use std::fs::File;
 
-fn main() {
-    // If any error would occur in inner_main(), print the error.
-    if let Err(err) = inner_main() {
-        eprintln!("{}", err);
-    }
-}
-
-fn inner_main() -> Result<(), Error> {
+fn main() -> Result<(), Error> {
     // clap app creation, with macros that read project information from Cargo.toml.
     let matches = App::new(crate_name!())
         .version(crate_version!())
